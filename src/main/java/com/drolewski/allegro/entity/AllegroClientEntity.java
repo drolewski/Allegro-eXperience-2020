c0f17c5e300ecdcbc62de2a7e034f2d54f0b1f02
rolex <drolewski@gmail.com>
2020-04-13 13:13:53 +0200

    Implement AllegroClient Service test

--- src/main/java/com/drolewski/allegro/entity/AllegroClientEntity.java
+++ src/main/java/com/drolewski/allegro/entity/AllegroClientEntity.java
@@ -1,10 +1,14 @@
 package com.drolewski.allegro.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "Allegro_Client")
 public class AllegroClientEntity {
